0fb0b1644c0683271e12465215ab330465225dd9
Derek Fujimoto <fujimoto@phas.ubc.ca>
2021-07-22 08:26:53 -0700

    v6.5.3

--- bdata/__init__.py
+++ bdata/__init__.py
@@ -7,6 +7,6 @@ from .exceptions import InputError, MinimizationError
 import os
 
 __all__ = ['bdata', 'bjoined', 'bmerged', 'life']
-__version__ = '6.5.2'
+__version__ = '6.5.3'
 __author__ = 'Derek Fujimoto'
 _mud_data = os.path.join(os.environ['HOME'], '.bdata')
