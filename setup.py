6f4be2c09389b82b8a0793c5f778921e32e03b48
Derek Fujimoto <d.fujimoto10@gmail.com>
2021-08-26 16:55:16 -0700

    Fixed install_requires strings

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
         "Operating System :: Microsoft :: Windows",
         "Development Status :: 5 - Production/Stable",
     ],
-    install_requires = ['cython> = 0.28', 'numpy> = 1.14', 'mud-py> = 1.2.6', 
-                      'requests> = 2.22.0', 'pandas> = 0.25', 'iminuit> = 2.6.1'],
+    install_requires = ['cython >= 0.28', 'numpy >= 1.14', 'mud-py >= 1.2.6', 
+                        'requests >= 2.22.0', 'pandas >= 0.25', 'iminuit >= 2.6.1'],
 )
 
