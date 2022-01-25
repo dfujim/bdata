75904ebc718fba6225cb1ba81a167fd2f150a4ce
Derek Fujimoto <fujimoto@phas.ubc.ca>
2022-01-25 14:35:53 -0800

    Added B0 and B1 calculators from bfit

--- bdata/__init__.py
+++ bdata/__init__.py
@@ -14,6 +14,6 @@ else:
     _homedir = os.environ['HOME']
 
 # define variables
-__all__ = ['bdata', 'bjoined', 'bmerged', 'life']
+__all__ = ['bdata', 'bjoined', 'bmerged', 'life', 'calc']
 __author__ = 'Derek Fujimoto'
 _mud_data = os.path.join(_homedir, '.bdata')
