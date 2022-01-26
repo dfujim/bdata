f466baf40d7448a259ba4c5dade9c6d53177d9dc
dfujimoto <dfujimoto@triumf.ca>
2022-01-26 13:10:10 -0800

    Added RMS errors

--- bdata/calc.py
+++ bdata/calc.py
@@ -1,7 +1,6 @@
 # Calculators for various quantities about the BNMR experiment
 # Derek Fujimoto 
 # Jan 2022
-
 import pandas as pd
 import numpy as np
 import os
@@ -14,41 +13,63 @@ def nqr_B0_hh6(amps=None, gauss=None):
     """
         Convert field to current and vice versa for bNQR HH6 magnet. 
         
+        Errors are derived from RMS of residuals for the overall fit
+        
         Input: either current (amps) OR field (gauss)
+        Returns: (other unit, error)
     """    
     if amps is not None: 
-        return amps*3.561129+1.574797
+        val = amps*3.56091338,+1.57410913
+        err = 0.006755
+        return (val, err)
     elif gauss is not None:
-        return (gauss-1.574797)/3.561129
+        val = (gauss-1.57410913)/3.56091338,
+        err = 0.001897
+        return (val, err)
 
 def nqr_B0_hh3(amps=None, gauss=None):
     """
         Convert field to current and vice versa for bNQR HH3 magnet. 
         
+        Errors are derived from RMS of residuals for the overall fit
+        
         Input: either current (amps) OR field (gauss)
+        Returns: (other unit, error)
     """    
     if amps is not None: 
-        return amps*2.21309+0.17476
+        val = amps*2.21309+0.17476
+        err = 0.05791
+        return (val, err)
     elif gauss is not None:
-        return (gauss-0.17476)/2.21309
+        val = (gauss-0.17476)/2.21309
+        err = 0.02617
+        return (val, err)
 
 def nmr_B1(mV=None, gauss=None, freq=41.27):
     """
         Convert field to peak-peak antenna voltage and vice versa for bNMR B1 magnet. 
         
+        Error unknown. Talk to Gerald.
+        
         Input: either voltage (mV) OR field (gauss)
                 nu: frequency of the signal in MHz
+        Returns: (other unit, error)
     """    
     if mV is not None: 
-        return mV*0.0396/freq
+        val = mV*0.0396/freq
+        err = 0
+        return (val, err)
     elif gauss is not None:
-        return gauss/0.0396*freq
+        val = gauss/0.0396*freq
+        err = 0
+        return (val, err)
 
 def nmr_atten(dac=None, power=None):
     """
         Convert DAC value to B1 power attenuation and vice versa for bNMR B1 magnet. 
         
         Input: either dac OR power (percent)
+        Returns: other unit
     """    
     
     data = get_atten_data()
@@ -73,6 +94,9 @@ def nmr_atten(dac=None, power=None):
                              right=2047))
 
 def get_atten_data():
+    """
+        Get the dataframe needed for the attenuation calculation
+    """
     data = pd.read_csv(data_location, comment="#")
 
     # normalize 
